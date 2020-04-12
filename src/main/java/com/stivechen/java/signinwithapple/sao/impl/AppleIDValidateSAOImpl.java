1eba33ef9470e95a8116a8d54867ee0de51bf792
stivechen <stivechen@gmail.com>
2020-04-12 22:12:30 +0800

    add 完善httpClient线程池

--- src/main/java/com/stivechen/java/signinwithapple/sao/impl/AppleIDValidateSAOImpl.java
+++ src/main/java/com/stivechen/java/signinwithapple/sao/impl/AppleIDValidateSAOImpl.java
@@ -12,17 +12,24 @@ import org.apache.commons.lang3.StringUtils;
 import org.apache.http.Consts;
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpStatus;
+import org.apache.http.client.config.RequestConfig;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpPost;
+import org.apache.http.config.SocketConfig;
 import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.impl.client.StandardHttpRequestRetryHandler;
 import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
 import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.protocol.HTTP;
 import org.apache.http.util.EntityUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
@@ -36,7 +43,7 @@ import java.util.List;
  * @create 2020/4/6
  * @since 1.0.0
  */
-@Service
+@Component
 @Slf4j
 public class AppleIDValidateSAOImpl implements AppleIDValidateSAO {
 
@@ -79,7 +86,7 @@ public class AppleIDValidateSAOImpl implements AppleIDValidateSAO {
                     log.error("AppleID validateAppleIDTokens http statusCode:{}", statusCode);
                     throw new SignInWithAppleException(AppleIDResponseCodeEnum.APPLEID_RESPONSE_ERROR);
 
-                }else {
+                } else {
                     responseResultData = EntityUtils.toString(httpEntity, Consts.UTF_8);
 
                     if (log.isDebugEnabled()) {
@@ -89,11 +96,12 @@ public class AppleIDValidateSAOImpl implements AppleIDValidateSAO {
             }
 
             if (StringUtils.isNoneEmpty(responseResultData)) {
-                appleIDResDTO = JSON.parseObject(responseResultData,AppleIDResDTO.class);
+                appleIDResDTO = JSON.parseObject(responseResultData, AppleIDResDTO.class);
             }
 
         } catch (IOException e) {
-            e.printStackTrace();
+            log.error("AppleID validateAppleIDTokens has IOException:{}", e.getMessage());
+
         } finally {
             //关闭，放回线程池
             if (null != httpResponse) {
@@ -107,4 +115,47 @@ public class AppleIDValidateSAOImpl implements AppleIDValidateSAO {
 
         return appleIDResDTO;
     }
+
+
+    /**
+     * ==========处理httpClient线程池，数据配置参考官方文档：http://hc.apache.org/index.html==========
+     */
+
+    @PostConstruct
+    public void init() {
+        //TODO 最好做成可配置
+        connManager = new PoolingHttpClientConnectionManager();
+        connManager.setMaxTotal(100);//最大连接数
+        connManager.setDefaultMaxPerRoute(100);//每个路由最大连接数
+        connManager.setDefaultSocketConfig(SocketConfig.custom().setSoTimeout(5000).build());//接受数据等待超时时间ms
+
+        RequestConfig requestConfig = RequestConfig.custom()
+                .setConnectTimeout(1000)//连接超时时间ms
+                .setSocketTimeout(5000)//读取数据超时ms
+                .setConnectionRequestTimeout(500)//从连接池获取连接超时时间ms
+                .build();
+        StandardHttpRequestRetryHandler retryHandler = new StandardHttpRequestRetryHandler(3, true);//重试次数
+
+        httpClient = HttpClients.custom()
+                .setConnectionManager(connManager)
+                .setDefaultRequestConfig(requestConfig)
+                .setRetryHandler(retryHandler)
+                .build();
+    }
+
+    @PreDestroy
+    public void destroy() {
+        try {
+            connManager.close();
+        } catch (Exception e) {
+            log.error("AppleID validateAppleIDTokens destroy connManager has error!");
+        }
+
+        try {
+            httpClient.close();
+        } catch (IOException e) {
+            log.error("AppleID validateAppleIDTokens destroy httpClient has error!");
+        }
+    }
+
 }
