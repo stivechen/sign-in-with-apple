87071104dcd0efbc738af343d4c8a96b8f76666c
stivechen <stivechen@gmail.com>
2020-04-18 21:55:11 +0800

    add 配置文件

--- src/test/java/com/stivechen/java/signinwithapple/SigninwithappleApplicationTests.java
+++ src/test/java/com/stivechen/java/signinwithapple/SigninwithappleApplicationTests.java
@@ -3,18 +3,27 @@ package com.stivechen.java.signinwithapple;
 import com.stivechen.java.signinwithapple.config.AppleIDConfig;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootTest
+@PropertySource(value = {"classpath:config/appleID.properties"})//使用@Value注解方式
 class SigninwithappleApplicationTests {
 
 	@Autowired
 	private AppleIDConfig appleIDConfig;
 
+	//Spring传统注解方式
+	@Value("${autoValue}")
+	private String autoValue;
+
 	@Test
 	void contextLoads() {
 		String privateKey = appleIDConfig.getPrivateKey();
 		System.out.println("privateKey:"+privateKey);
+
+		System.out.println("autoValue:"+autoValue);
 	}
 
 }
